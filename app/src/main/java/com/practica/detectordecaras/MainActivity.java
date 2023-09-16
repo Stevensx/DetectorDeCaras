1b3f1c796074885f77782efcd7853220a75e09e3
Stevensx <136100719+Stevensx@users.noreply.github.com>
2023-09-16 18:57:18 -0500

    Update

--- app/src/main/java/com/practica/detectordecaras/MainActivity.java
+++ app/src/main/java/com/practica/detectordecaras/MainActivity.java
@@ -1,120 +1,245 @@
 package com.practica.detectordecaras;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.media.ImageReader;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.practica.detectordecaras.ml.FlowerModel;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.mlkit.vision.common.InputImage;
-import com.google.mlkit.vision.label.ImageLabel;
-import com.google.mlkit.vision.label.ImageLabeler;
-import com.google.mlkit.vision.label.ImageLabeling;
-import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
+import com.practica.detectordecaras.ml.Jugadores;
 
+import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.image.TensorImage;
-import org.tensorflow.lite.support.label.Category;
-import org.w3c.dom.Text;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity implements OnSuccessListener<Text>, OnFailureListener, ImageReader.OnImageAvailableListener {
-    public static int REQUEST_CAMERA = 111;
-    public static int REQUEST_GALLERY = 222;
-    public Bitmap mSelectedImage;
-    public ImageView mImageView;
-    public TextView txtResults;
-    public ImageLabeler imageLabeler;
+import com.practica.detectordecaras.R;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static Intent newIntent(Context context) { Log.d(TAG,"newIntent()");
+        return new Intent(context.getApplicationContext(), MainActivity.class)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    }
+    private static final int PERMISSION_STATE = 0;
+    private static final int CAMERA_REQUEST = 1;
+    private static final int GALLERY_REQUEST = 2;
+    public static final int botonEscanear = 123;  // Ejemplo de constante
+
+
+    private Button imgCamera;
+    private ImageView imgResult;
+    private Button btnPredict;
+    private Button btnSelect;
+    private TextView txtPrediction;
+    private Bitmap bitmap;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { Log.d(TAG,"onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtResults = findViewById(R.id.txtResults);
-        mImageView = findViewById(R.id.image_view);
-    }
-    public void abrirGaleria (View view){
-        Intent i = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(i, REQUEST_GALLERY);
-    }
-    public void abrirCamera (View view){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, REQUEST_CAMERA);
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && null != data) {
-            try {
-                if (requestCode == REQUEST_CAMERA)
-                    mSelectedImage = (Bitmap) data.getExtras().get("data");
-                else
-                    mSelectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+        imgCamera = (Button) findViewById(R.id.botonCaptura);
+        imgResult = (ImageView) findViewById(R.id.ResultadoImagen);
+        txtPrediction = (TextView) findViewById(R.id.Resultado);
+        btnPredict = (Button) findViewById(R.id.ScanearBoton);
+        btnSelect = (Button) findViewById(R.id.GaleriaBoton);
 
-                mImageView.setImageBitmap(mSelectedImage);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        btnSelect.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.ScanearBoton:
+                predict();
+                break;
+            case R.id.botonCaptura:
+                launchCamera();
+                break;
+            case R.id.GaleriaBoton:
+                openGallery();
+                break;
+            default:
+                break;
         }
     }
-    public void PersonalizedModel(View v) {
-        try {
-            FlowerModel model = FlowerModel.newInstance(getApplicationContext());
-            TensorImage image = TensorImage.fromBitmap(mSelectedImage);
 
-            FlowerModel.Outputs outputs = model.process(image);
+    @Override
+    protected void onResume() { Log.d(TAG,"onResume()");
+        super.onResume();
+        btnPredict.setOnClickListener(this::onClick);
+        imgCamera.setOnClickListener(this::onClick);
+        checkPermissions();
+    }
 
-            List<Category> probability = outputs.getProbabilityAsCategoryList();
-            Collections.sort(probability, new CategoryComparator());
+    @Override
+    protected void onPause() { Log.d(TAG,"onPause()");
+        super.onPause();
+        btnPredict.setOnClickListener(null);
+        imgCamera.setOnClickListener(null);
+    }
 
-            String res="";
-            for (int i = 0; i < probability.size(); i++) {
-                res = res + probability.get(i).getLabel() +  " " +  probability.get(i).getScore()*100 + " % \n";
-            }
+    private void launchCamera() { Log.d(TAG,"launchCamera()");
+        startActivityForResult(new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE), CAMERA_REQUEST);
+    }
 
-            txtResults.setText(res);
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY_REQUEST);
+    }
+
+
+    private void predict() { Log.d(TAG,"predict()");
+        bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+        try { Log.d(TAG,"try");
+            Jugadores model = Jugadores.newInstance(getApplicationContext());
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.UINT8);
+            TensorImage tensorImage = new TensorImage(DataType.UINT8);
+            tensorImage.load(bitmap);
+            ByteBuffer byteBuffer = tensorImage.getBuffer();
+
+            inputFeature0.loadBuffer(byteBuffer);
+            // Runs model inference and gets result.
+            Jugadores.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+            // Releases model resources if no longer used.
             model.close();
+            txtPrediction.setText(getMax(outputFeature0.getFloatArray()));//txtPrediction.setText(outputFeature0.getFloatArray()[0] + "\n" + outputFeature0.getFloatArray()[1] + "\n" + outputFeature0.getFloatArray()[2]);
+            getMax(outputFeature0.getFloatArray());
+            Log.d("Result",Arrays.toString(outputFeature0.getFloatArray()));
         } catch (IOException e) {
-            txtResults.setText("Error al procesar Modelo");
+            Log.e(TAG,"IOException " + e.getMessage());
         }
     }
-    class CategoryComparator implements java.util.Comparator<Category> {
-        @Override
-        public int compare(Category a, Category b) {
-            return (int)(b.getScore()*100) - (int)(a.getScore()*100);
+
+    private String getMax(float[] outputs) {
+        if (outputs.length >= 5) {
+            float maxOutput = outputs[0];
+            int maxIndex = 0;
+
+            for (int i = 1; i < outputs.length; i++) {
+                if (outputs[i] > maxOutput) {
+                    maxOutput = outputs[i];
+                    maxIndex = i;
+                }
+            }
+
+            String[] classes = {"Leonel Messi", "Cristiano Ronaldo", "Neymar", "Gerard Piqué", "Kun Aguero"};
+            if (maxIndex < classes.length) {
+                return classes[maxIndex];
+            }
+        }
+        return "Perdón Karnal no conozco a ese"; // Retorna cadena vacía si no se puede determinar una clase
+    }
+
+    private void checkPermissions() {
+        String[] manifestPermissions;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            manifestPermissions = new String[]{
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
+        } else {
+            manifestPermissions = new String[] {
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
+        }
+        for (String permission : manifestPermissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG,"Permission Granted " + permission);
+            }
+            if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+                Log.d(TAG,"Permission Denied " + permission);
+                requestPermissions();
+            }
+        }
+    }
+
+    private void requestPermissions() { Log.d(TAG, "requestPermissions()");
+        String[] manifestPermissions;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            manifestPermissions = new String[]{
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
+        } else {
+            manifestPermissions = new String[] {
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            };
         }
 
-
-
-
-
-
-    }
-    @Override
-    public void onFailure(@NonNull Exception e) {
-
+        ActivityCompat.requestPermissions(
+                this,
+                manifestPermissions,
+                PERMISSION_STATE
+        );
     }
 
     @Override
-    public void onSuccess(Text text) {
-        txtResults.setText("Imagen Identificada con éxito");
-
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "PermissionsResult requestCode " + requestCode);
+        Log.d(TAG, "PermissionsResult permissions " + Arrays.toString(permissions));
+        Log.d(TAG, "PermissionsResult grantResults " + Arrays.toString(grantResults));
+        if (requestCode == PERMISSION_STATE) {
+            for (int grantResult : grantResults) {
+                switch (grantResult) {
+                    case PackageManager.PERMISSION_GRANTED:
+                        Log.d(TAG, "PermissionsResult grantResult Allowed " + grantResult);
+                        break;
+                    case PackageManager.PERMISSION_DENIED:
+                        Log.d(TAG, "PermissionsResult grantResult Denied " + grantResult);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 
     @Override
-    public void onImageAvailable(ImageReader reader) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            try {
+                // Obtén la imagen a partir de la URI
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                imgResult.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                Log.e(TAG, "Error al cargar la imagen desde la galería: " + e.getMessage());
+            }
+        } else if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK && data != null) {
+            bitmap = (Bitmap) data.getExtras().get("data");
+            imgResult.setImageBitmap(bitmap);
+        }
     }
-
 }
